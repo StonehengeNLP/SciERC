579764218099ff0454b266d79d9594df243ad6e2
tongplw <bangkok.tong@gmail.com>
2020-10-12 20:48:30 +0700

    fixed bugs and updated storage

--- scripts/build_custom_kernels.sh
+++ scripts/build_custom_kernels.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-TF_ORIGIN=$(python -c 'import importlib; print(importlib.util.find_spec("tensorflow").origin)')
+TF_ORIGIN=$(python3 -c 'import importlib; print(importlib.util.find_spec("tensorflow").origin)')
 TF_DIR=$(dirname $TF_ORIGIN)
 TF_INC=$TF_DIR/include
 TF_LIB=$TF_DIR
 
 ldd srl_kernels.so
-sudo ln -s libtensorflow_framework.so.1 -f libtensorflow_framework.so
+ln -s libtensorflow_framework.so.1 -f libtensorflow_framework.so
 
 g++ -std=c++11 -shared srl_kernels.cc -o srl_kernels.so \
     -I$TF_INC -I$TF_INC/external/nsync/public \
--- scripts/fetch_required_data.sh
+++ scripts/fetch_required_data.sh
@@ -10,32 +10,33 @@ cd embeddings
 
 if [ ! -f "glove.840B.300d.txt" ]; then
   # wget http://nlp.stanford.edu/data/glove.840B.300d.zip
-  wget https://storage.googleapis.com/mag-tongplw/mag/glove.840B.300d.zip
+  wget https://storage.googleapis.com/scierc/glove.840B.300d.zip
   unzip glove.840B.300d.zip
   rm glove.840B.300d.zip
 fi
 
 if [ ! -f "glove_50_300_2.txt" ]; then
   # wget https://dada.cs.washington.edu/qasrl/data/glove_50_300_2.zip
-  wget https://storage.googleapis.com/mag-tongplw/mag/glove_50_300_2.zip
+  wget https://storage.googleapis.com/scierc/glove_50_300_2.zip
   unzip glove_50_300_2.zip
   rm glove_50_300_2.zip
 fi
 
 cd $OLDPWD
 
-DATA_PATH="./data/"
+DATA_PATH="./data"
 if [ ! -d $DATA_PATH ]; then
   mkdir -p $DATA_PATH
 fi
 
 # Get srl-conll package.
-if [ ! -d "processed_data" ]; then
-  wget -O "${DATA_PATH}/sciERC_processed.tar.gz" http://ssli.ee.washington.edu/tial/projects/sciIE/data/sciERC_processed.tar.gz
+if [ ! -d "${DATA_PATH}/processed_data" ]; then
+  # wget -O "${DATA_PATH}/sciERC_processed.tar.gz" http://ssli.ee.washington.edu/tial/projects/sciIE/data/sciERC_processed.tar.gz
+  wget -O "${DATA_PATH}/sciERC_processed.tar.gz" https://storage.googleapis.com/scierc/sciERC_processed.tar.gz
   tar xf "${DATA_PATH}/sciERC_processed.tar.gz" -C "${DATA_PATH}"
   rm "${DATA_PATH}/sciERC_processed.tar.gz"
 fi
 
-python scripts/filter_embeddings.py ${EMB_PATH}/glove.840B.300d.txt ${EMB_PATH}/glove.840B.300d.txt.filtered ${DATA_PATH}/processed_data/json/train.json ${DATA_PATH}/processed_data/json/dev.json
+python3 scripts/filter_embeddings.py ${EMB_PATH}/glove.840B.300d.txt ${EMB_PATH}/glove.840B.300d.txt.filtered ${DATA_PATH}/processed_data/json/train.json ${DATA_PATH}/processed_data/json/dev.json
 
-python scripts/get_char_vocab.py 
+python3 scripts/get_char_vocab.py 
