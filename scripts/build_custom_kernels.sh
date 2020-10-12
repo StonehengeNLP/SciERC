0d393ab2e136dee95e5e54006f2e9c47f44ee3f7
tongplw <bangkok.tong@gmail.com>
2020-10-12 22:43:03 +0700

    added requirements.txt and fixed a bug

--- scripts/build_custom_kernels.sh
+++ scripts/build_custom_kernels.sh
@@ -5,10 +5,7 @@ TF_DIR=$(dirname $TF_ORIGIN)
 TF_INC=$TF_DIR/include
 TF_LIB=$TF_DIR
 
-ldd srl_kernels.so
-ln -s libtensorflow_framework.so.1 -f libtensorflow_framework.so
-
 g++ -std=c++11 -shared srl_kernels.cc -o srl_kernels.so \
     -I$TF_INC -I$TF_INC/external/nsync/public \
     -L$TF_LIB -ltensorflow_framework \
-    -fPIC -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+    -fPIC -O2 # -D_GLIBCXX_USE_CXX11_ABI=0
