072268a1f590fe481a55c8dd1ed5d14947dc20e7
tongplw <bangkok.tong@gmail.com>
2020-10-13 23:19:44 +0700

    update new model's link

--- scripts/fetch_coref_model.sh
+++ scripts/fetch_coref_model.sh
@@ -2,5 +2,5 @@ if [ ! -d "./logs" ]; then
     mkdir -p "./logs"
 fi
 
-wget http://nlp.cs.washington.edu/sciIE/models/scientific_best_coref.zip
+wget https://storage.googleapis.com/scierc/scientific_best_coref.zip
 unzip scientific_best_coref.zip -d ./logs
